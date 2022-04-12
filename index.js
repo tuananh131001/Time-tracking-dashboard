e45ca8b6694ca591ae49548a3766329c1cc150b2
William <s3864077@rmit.edu.vn>
2022-04-12 08:55:56 +0700

    len

---
+++ index.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll(".changeTime");
+console.log(buttons);
+
+buttons.forEach(btn => {
+    btn.addEventListener('click', (e)=>{
+        console.log("hello")
+    });
+    
+})
+
+
